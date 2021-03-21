245afa6b502be5bd77f39d7688fbb5b42b9e4d32
Sang Le <sang.le@notabasement.com>
2021-03-21 17:47:41 +0700

    Adjust package

--- Package.swift
+++ Package.swift
@@ -9,8 +9,15 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "GoogleMobileAds",
-            targets: ["GoogleMobileAds"]),
-    ],
+            targets: [
+                "GoogleAppMeasurement",
+                "GoogleMobileAds",
+                "GoogleUtilities",
+                "nanopb",
+                "PromisesObjC",
+                "UserMessagingPlatform"
+            ]
+    )],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
@@ -18,11 +25,11 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "GoogleMobileAds",
-            dependencies: []),
-        .testTarget(
-            name: "GoogleMobileAdsTests",
-            dependencies: ["GoogleMobileAds"]),
+        .binaryTarget(name: "GoogleAppMeasurement", path: "Frameworks/GoogleAppMeasurement.xcframework"),
+        .binaryTarget(name: "PromisesObjC", path: "Frameworks/PromisesObjC.xcframework"),
+        .binaryTarget(name: "GoogleMobileAds", path: "Frameworks/GoogleMobileAds.xcframework"),
+        .binaryTarget(name: "GoogleUtilities", path: "Frameworks/GoogleUtilities.xcframework"),
+        .binaryTarget(name: "nanopb", path: "Frameworks/nanopb.xcframework"),
+        .binaryTarget(name: "UserMessagingPlatform", path: "Frameworks/UserMessagingPlatform.xcframework"),
     ]
 )
