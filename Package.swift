6cecff069c0fdb35ff1c39b1002fd28f1c095f6c
Sang Le <sang.le@notabasement.com>
2021-03-22 17:15:33 +0700

    Remove Utilities & PromiseObjC

--- Package.swift
+++ Package.swift
@@ -11,8 +11,6 @@ let package = Package(
             name: "GoogleMobileAds",
             targets: [
                 "GoogleMobileAds",
-                "GoogleUtilities",
-                "PromisesObjC",
                 "UserMessagingPlatform"
             ]
     )],
@@ -23,9 +21,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .binaryTarget(name: "PromisesObjC", path: "Frameworks/PromisesObjC.xcframework"),
         .binaryTarget(name: "GoogleMobileAds", path: "Frameworks/GoogleMobileAds.xcframework"),
-        .binaryTarget(name: "GoogleUtilities", path: "Frameworks/GoogleUtilities.xcframework"),
         .binaryTarget(name: "UserMessagingPlatform", path: "Frameworks/UserMessagingPlatform.xcframework"),
     ]
 )
